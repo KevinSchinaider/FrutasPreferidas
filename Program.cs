be0ad03def2fbe1fa5ad7f39137949a55e360cdc
KevinSchinaider <kevinschinaider05@outlook.com>
2022-09-17 10:11:29 -0300

    Solucão exercício

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("--- Minhas frutas preferidas ---\n");
+Console.WriteLine("");
+Console.Write("1. Manga\n");
+Console.Write("2. Maça\n");
+Console.Write("3. Banana\n");
+Console.Write("4. Melancia\n");
+Console.Write("5. Laranja\n");
